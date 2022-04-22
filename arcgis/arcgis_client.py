5fff06c8d240e244894bde3151327c2c56769d94
Nicolas de la Fuente <ndelafuente@sandiego.edu>
2022-04-21 20:55:46 -0700

    complete rewrite of arcgis_client

--- arcgis/arcgis_client.py
+++ arcgis/arcgis_client.py
@@ -1,55 +1,15 @@
-from asyncio.log import logger
-import json
-import arcgis
-import requests
-import config as cfg
-from os import system
+import config as cf
 
-class ApiError(BaseException):
-    def __init__(self, error_response):
-        self.message = error_response
 
-    def __repr__(self):
-        return str(self.message)
+class ArcgisClient:
+    """
+    This class will serve as a way to interface the ArcGIS REST API
+    """
+    pass
 
-def get_token(client_id, client_secret):
-    params = {
-        'client_id': client_id,
-        'client_secret': client_secret,
-        'grant_type': "client_credentials"
-    }
-    request = requests.get('https://www.arcgis.com/sharing/rest/oauth2/token',
-                        params=params)
-
-    response = request.json()
-    return parse_response(response, "access_token")
-
-def parse_response(response: dict, key: str):
-    if key in response:
-        return response[key]
-    elif 'error' in response:
-        raise ApiError(response['error'])
-    else:
-        raise ApiError("Unsopported response type:" + str(response))
-
-def authorize():
-    parameters = {
-        'client_id': cfg.CLIENT_ID,
-        'response_type': 'code',
-        'expiration': 60,
-        'redirect_uri': 'urn:ietf:wg:oauth:2.0:oob'
-    }
-
-    # Ask the portal to authorize us
-    url = 'https://' + cfg.PORTAL_NAME + '.maps.arcgis.com/sharing/rest/oauth2/authorize'
-    
-    response = requests.get(url, params=parameters)
-    system(f"open {response.url}")
-    content = response.text
-
-    outfile = "response.html"
-    file = open(outfile, 'w')
-
-    print(content, file=file)
-    # system(f"open {outfile}")
-
+def sandbox():
+    from arcgis.gis import GIS
+    gis = GIS(cf.AGOL_URL, username=cf.USER, password=cf.PASS)
+    print(gis.users.me)
+    # tmp = gis.content.search(query="owner:NDELAFUENTE@sandiego.edu_usandiego")
+    # print(tmp, file = open('tmp.json', 'w'))
