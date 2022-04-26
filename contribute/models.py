9d2770ea5a9186982f9df35d063a13ff1d9b7bb1
Nicolas de la Fuente <ndelafuente@sandiego.edu>
2022-04-26 15:20:53 -0700

    complete overhaul of models

--- contribute/models.py
+++ contribute/models.py
@@ -2,6 +2,7 @@ from pickle import NONE, TRUE
 from ssl import Options
 from django.db import models
 
+
 class CONTACT(models.Model):
     """
     Contact information for person who is submitting the form
@@ -16,42 +17,71 @@ class CONTACT(models.Model):
 
     # created_at = models.DateTimeField(auto_now_add=True)
 
-
 class ORGANIZATION(models.Model):
     """
     Information for the organization submitting the form
     """
-    org_name = models.CharField(max_length=100)
-    org_class = models.CharField(max_length=100)
-    org_description = models.CharField(max_length=500)
-    org_website = models.CharField(max_length=100)
-    org_email = models.EmailField(max_length=100)
-    org_address = models.CharField(max_length=100)
-    org_city = models.CharField(max_length=50)
-    org_state = models.CharField(max_length=10)
-    org_zip = models.IntegerField()
-    org_country = models.CharField(max_length=10)
-    org_title = models.CharField(max_length=10)
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+
+    # Contact info
+    website = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+
+    # Location
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=10)
+    zip = models.IntegerField()
+    country = models.CharField(max_length=10)
+
+    # title = models.CharField(max_length=10)
 
     def __str__(self):
-        return self.org_name
+        return self.name
 
+class LOCATION(models.Model):
+    """
+    Location model
+    """
+    location_name = models.CharField(max_length=200)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
 
 class PROJECT(models.Model):
     """
     Information about the projects/monitoring status of the research
+    
+    TODO:
+    @All: How should foreign keys be handled?
+    @GingerMcKelvey: How should we handle parameters monitored?
+        - There should be a predetermined list for the user to choose from.
+        - And the user should be able to select multiple at once
     """
-    #not sure if this is how to correctly handle foreign keys
+    # Auto-generated data
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # 'Who'
     project_name = models.CharField(max_length=200)
-    project_org = models.ForeignKey(ORGANIZATION, on_delete=models.CASCADE)
-    project_fa = models.CharField(max_length=200)
-    project_loc = models.CharField(max_length=200)
-    loc_lat = models.FloatField()
-    loc_long = models.FloatField()
-    active_status = models.BooleanField(unique=TRUE) # true is still going false is completed
-    tools_used = models.CharField(max_length=200)
-    project_startdate = models.DateField()
-    project_enddate = models.DateField(default=NONE)
+    fk_organization = models.ForeignKey(ORGANIZATION, on_delete=models.CASCADE)
+    fk_contact = models.ForeignKey(CONTACT, on_delete=models.CASCADE)
+    funding_agencies = models.CharField(max_length=200)
+    
+    # 'What'
+    parameters_monitored = models.CharField(max_length=200)
+
+    # 'Where'
+    fk_location = models.ForeignKey(LOCATION, on_delete=models.CASCADE)
+
+    # 'When'
+    is_active = models.BooleanField(unique=TRUE)
+    start_date = models.DateField()
+    end_date = models.DateField(default=NONE)
+
+    # 'Why'
+    purpose = models.TextField(max_length=400)
+
     
     def __str__(self):
         return self.project_name
